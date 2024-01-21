58500a2f6ab0e11fc3e13ff6f6c56221c39bbbff
Danielle096 <157294641+Danielle096@users.noreply.github.com>
2024-01-21 19:44:58 +0300

    Update downloader-play_v2.js

--- plugins/downloader-play_v2.js
+++ plugins/downloader-play_v2.js
@@ -87,7 +87,7 @@ import axios from 'axios';
 const handler = async (m, {command, usedPrefix, conn, text}) => {
   if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝙻𝙰 𝙲𝙰𝙽𝙲𝙸𝙾𝙽 𝙵𝙰𝙻𝚃𝙰𝙽𝚃𝙴, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝙸𝙽𝙶𝚁𝙴𝚂𝙴 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 𝙼𝙰𝚂 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴/𝚃𝙸𝚃𝚄𝙻𝙾 𝙳𝙴 𝙰𝙻𝙶𝚄𝙽𝙰 𝙲𝙰𝙽𝙲𝙸𝙾𝙽 𝙾 𝚅𝙸𝙳𝙴𝙾 𝙳𝙴 𝚈𝙾𝚄𝚃𝚄𝙱𝙴*\n\n*—◉ 𝙴𝙹𝙴𝙼𝙿𝙻𝙾:*\n*${usedPrefix + command} Good Feeling - Flo Rida*`;
   try {
-    if (command == 'play.1') {
+    if (command == 'play1') {
       conn.reply(m.chat, `*_⏳Sᴇ ᴇsᴛᴀ ᴘʀᴏᴄᴇsᴀɴᴅᴏ Sᴜ ᴀᴜᴅɪᴏ...⏳_*`, m);
       try {
         const mediaa = await ytPlay(text);
@@ -103,7 +103,7 @@ const handler = async (m, {command, usedPrefix, conn, text}) => {
         if (!aa_1) aa_1 = await conn.sendFile(m.chat, json.result.audio, 'error.mp3', null, m, false, {mimetype: 'audio/mpeg'});
       }
     }
-    if (command == 'play.2') {
+    if (command == 'play2') {
       conn.reply(m.chat, `*_⏳Sᴇ ᴇsᴛᴀ ᴘʀᴏᴄᴇsᴀɴᴅᴏ Sᴜ ᴠɪᴅᴇᴏ...⏳_*`, m);
       try {
         const mediaa = await ytPlayVid(text);
@@ -123,7 +123,7 @@ const handler = async (m, {command, usedPrefix, conn, text}) => {
 };
 handler.help = ['play.1', 'play.2'].map((v) => v + ' <texto>');
 handler.tags = ['downloader'];
-handler.command = ['play.1', 'play.2'];
+handler.command = ['play1', 'play2'];
 export default handler;
 
 function bytesToSize(bytes) {
