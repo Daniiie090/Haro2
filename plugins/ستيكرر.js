039265ef1c9721ae0911beaa76812658d68f1543
Danielle096 <157294641+Danielle096@users.noreply.github.com>
2024-01-22 21:19:25 +0300

    Update ستيكرر.js

--- plugins/ستيكرر.js
+++ plugins/ستيكرر.js
@@ -2,12 +2,12 @@ import { Sticker } from 'wa-sticker-formatter'
 
 let handler = async (m, { conn }) => {
   
-  let vid = 'https://telegra.ph/file/9953943f2c9d22e34844d.mp4 ';
+  let img = 'https://telegra.ph/file/c57ba3c869d3c6d8bda7d.png';
   let stiker = await createSticker(false, img, '', author, 20)
   
   m.reply(stiker)
 };
-handler.customPrefix = /^(تست)$/i;
+handler.customPrefix = /^(احبك)$/i;
 handler.command = new RegExp;
 
 export default handler;
