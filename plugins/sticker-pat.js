94366d34020015c27c6bd3be8198a08eb99228f1
Danielle096 <157294641+Danielle096@users.noreply.github.com>
2024-01-23 14:29:51 +0300

    Update sticker-pat.js

--- plugins/sticker-pat.js
+++ plugins/sticker-pat.js
@@ -1,16 +1,23 @@
-import {sticker} from '../lib/sticker.js';
-import fetch from 'node-fetch';
-import MessageType from '@whiskeysockets/baileys';
-const handler = async (m, {conn}) => {
-  try {
-    if (m.quoted?.sender) m.mentionedJid.push(m.quoted.sender);
-    if (!m.mentionedJid.length) m.mentionedJid.push(m.sender);
-    const res = await fetch('https://api.waifu.pics/sfw/pat');
-    const json = await res.json();
-    const {url} = json;
-    const stiker = await sticker(null, url, `+${m.sender.split('@')[0]} le dio palmaditas a ${m.mentionedJid.map((user)=>(user === m.sender)? 'alguien ': `+${user.split('@')[0]}`).join(', ')}`);
-    conn.sendFile(m.chat, stiker, null, {asSticker: true});
-  } catch (e) { }
-};
-handler.command = /^(pat|palmaditas|cariños|mimos|patt)$/i;
-export default handler;
+import { addExif } from '../lib/sticker.js'
+let handler = async (m, { conn, text }) => {
+if (!m.quoted) throw '*قم بالرد على الملصق الذي تريد إضافة حزمة إليه*'
+let stiker = false
+try {
+let [packname, ...author] = text.split('|')
+author = (author || []).join('|')
+let mime = m.quoted.mimetype || ''
+if (!/webp/.test(mime)) throw '*قم بالرد على الملصق الذي تريد إضافة حزمة إليه*'
+let img = await m.quoted.download()
+if (!img) throw '*امنح الملصق الذي تريده لإضافة حزمة واسم*'
+stiker = await addExif(img, packname || '', author || '')
+} catch (e) {
+console.error(e)
+if (Buffer.isBuffer(e)) stiker = e
+} finally {
+if (stiker) conn.sendFile(m.chat, stiker, 'wm.webp', '', m, false, { asSticker: true })
+else throw '*آسف ، شيء ما خاطئ ..تحقق من أنك استجابت لملصق وأضفت اسم حزمة واسم حزمة*'
+}}
+handler.help = ['wm <packname>|<author>']
+handler.tags = ['sticker']
+handler.command = /^سرقه|حقوق$/i
+export default handler
