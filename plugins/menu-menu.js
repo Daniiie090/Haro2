4c67fbbc5c38b0153f2723d9b904b3ab2ceab4ad
Dnaielle <157284901+Dnaielle@users.noreply.github.com>
2024-02-09 00:03:06 +0300

    Update menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -1,15 +1,27 @@
-const dir = [
-    'https://telegra.ph/file/9274850cb546d86b98c9f.mp4',
-    'https://telegra.ph/file/07f90949b124189139241.mp4',
-    'https://telegra.ph/file/024a664c578806c019b8c.mp4',
-];
-
-let handler = async (m, { conn }) => {
-    const name = conn.getName(m.sender);
-    const videoUrl = pickRandom(dir);
-    const taguser = '@' + m.sender.split("@s.whatsapp.net")[0];
+import fetch from 'node-fetch';
+const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems}) => {
+  if (usedPrefix == 'a' || usedPrefix == 'A') return;
+  try {
+    const pp = imagen4;
+    // let vn = './media/menu.mp3'
+    const img = './Menu2.jpg';
+    const d = new Date(new Date + 3600000);
+    const locale = 'ar';
+    const week = d.toLocaleDateString(locale, {weekday: 'long'});
+    const date = d.toLocaleDateString(locale, {day: 'numeric', month: 'long', year: 'numeric'});
+    const _uptime = process.uptime() * 1000;
+    const uptime = clockString(_uptime);
+    const user = global.db.data.users[m.sender];
+    const {money, joincount} = global.db.data.users[m.sender];
+    const {exp, limit, level, role} = global.db.data.users[m.sender];
+    const rtotalreg = Object.values(global.db.data.users).filter((user) => user.registered == true).length;
+    const rtotal = Object.entries(global.db.data.users).length || '0'
     const more = String.fromCharCode(8206);
-    const teks = `${pickRandom([`╭═══〘 ✯✯✯✯✯✯✯✯✯ ═●▬▬▬ஜƸ̵̡⁠Ӝ̵̨̄⁠Ʒஜ▬▬▬●
+    const readMore = more.repeat(850);
+    const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
+    const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
+    const document = doc[Math.floor(Math.random() * doc.length)];
+    const str = `╭═══〘 ✯✯✯✯✯✯✯✯✯ ═●▬▬▬ஜƸ̵̡⁠Ӝ̵̨̄⁠Ʒஜ▬▬▬●
 *❘╼╼𓏪 قائـمـة 📜 الاوامـر 𓏪╾╾❘*
 ── • ◈ • ──
 *🎶✬⃝┇اهلا👋* ${taguser}
@@ -62,8 +74,8 @@ let handler = async (m, { conn }) => {
 *⏎* يجيبلك ايه من قرأن كريم
 *✎╎🗣️ ⇠〘 .حديث 〙*    
 *⏎* البوت يرسل احاديث
-*✎╎📖 ⇠〘 اسماء-الله 〙*    
-*⏎  يأتي لك بأسماء الله الحسنى مع شرح الاسم
+*✎╎📖 ⇠〘 .سوره 〙*    
+*⏎  اكتب رقم السوره وبعدها رقم الايه يجيبلك الايه مثال .سوره 2 255
 *✎╎🕋 ⇠〘 .ايه 〙*    
 *⏎* اكتب رقم سوره وبعدها رقم الايه بس بصوت شيخ مختلف
 ━ ── • ⟐ • ── ━
@@ -104,8 +116,8 @@ let handler = async (m, { conn }) => {
 *⏎* تحميل اي مقطع يوتيوب بل اسم بسرعة
 *✎╎🎶 ⇠〘.play〙*
 *⏎*يحملك اي اغنيه من يوتيوب بل اسم بسرعة ودقة
-*✎╎🎶 ⇠〘سبوت 〙*
-*⏎*ا امر خاص بتحميل من سبوتيفاي
+*✎╎🎶 ⇠〘.play.2  〙*
+*⏎*الامر خاص بتحميل من يوتيوب عن طريق رابط او اسم والرقم واحد بدل اثنان لتحميل صوت بدل مقطع
 *✎╎🎶 ⇠〘.yta2〙*
 *⏎*هذا لامر للتحميل من يوتيوب على شكل مستند بدقة عالية
 *✎╎🎶 ⇠〘.yta〙*
@@ -129,10 +141,6 @@ let handler = async (m, { conn }) => {
 *⏎*  اسأل البوت اي سؤال
 *✎╎👾 ⇠〘 .هارو 〙*    
 *⏎* ذكاء اصطناعي جانبي أسأله اي سؤال
-*✎╎👾 ⇠〘 .ياسو 〙*    
-*⏎* ذكاء اصتناعي متطور
-*✎╎👾 ⇠〘. تخيل〙*    
-*⏎* يحول النص للكلام استخدم اللغة الانغليزية افضل
 ━ ── • ⟐ • ── ━
 *˼‏🕹️˹ قـسـم العاب1 سؤال وإجابه╿↶*
 ━ ── • ⟐ • ── ━
@@ -313,20 +321,31 @@ let handler = async (m, { conn }) => {
 *❷ - استخدام البوت بشكل متوازن و بدون تسبب ازعاج للاعضاء.*
  ╰⋅ ⋅ ── ⋅ ⋅ ── ✩ ── ⋅ ⋅ ── ⋅ ⋅ ╯
 
-*~.¸¸ ❝ GojoXBots❝ ¸¸.~*`])}`.trim();
+*~.¸¸ ❝ GojoXBots❝ ¸¸.~*`.trim();
+    if (m.isGroup) {
+      // await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
+      const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
+      conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: m});
+    } else {
+      // await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
+      const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
+      conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: fkontak2});
+    }
+  } catch {
+    conn.reply(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝙻 𝙼𝙴𝙽𝚄 𝚃𝙸𝙴𝙽𝙴 𝚄𝙽 𝙴𝚁𝚁𝙾𝚁 𝚈 𝙽𝙾 𝙵𝚄𝙴 𝙿𝙾𝚂𝙸𝙱𝙻𝙴 𝙴𝙽𝚅𝙸𝙰𝚁𝙻𝙾, 𝚁𝙴𝙿𝙾𝚁𝚃𝙴𝙻𝙾 𝙰𝙻 𝙿𝚁𝙾𝙿𝙸𝙴𝚃𝙰𝚁𝙸𝙾 𝙳𝙴𝙻 𝙱𝙾𝚃*', m);
+  }
 
-    conn.sendMessage(m.chat, {
-        video: { url: videoUrl },
-        caption: teks,
-        gifPlayback: true,
-        gifAttribution: 0
-    }, { quoted: m });
-};
+conn.sendFile(m.chat, 'https://file.io/vT8PSQr6XBV7', 'sticker.webp', '', m, { asSticker: true });
 
-handler.command = ['اوامر'];
+}; 
 
+handler.command = /^(menu|a|memu|memú|اوامر|info|comandos|allmenu|menu1.2|ayuda|commands|commandos|cmd)$/i;
+handler.exp = 50;
+handler.fail = null;
 export default handler;
-
-function pickRandom(list) {
-    return list[Math.floor(Math.random() * list.length)];
+function clockString(ms) {
+  const h = isNaN(ms) ? '--' : Math.floor(ms / 3600000);
+  const m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60;
+  const s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60;
+  return [h, m, s].map((v) => v.toString().padStart(2, 0)).join(':');
 }
