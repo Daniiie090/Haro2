13618c6b44fdf8f100bc200696b17b8cad7b0d78
Dnaielle <157284901+Dnaielle@users.noreply.github.com>
2024-02-08 14:58:29 +0300

    Update _detectEvents.js

--- plugins/_detectEvents.js
+++ plugins/_detectEvents.js
@@ -19,41 +19,41 @@ export async function before(m, {conn, participants}) {
   const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
 
   if (chat.detect2 && m.messageStubType == 29) {
-    let txt1 = `*💠مبارك🥳 تم تعيين مشرف جديد في المجموعة!!💠.*\n\n`;
-    txt1 += `*◦  المجموعة:* ${groupName}\n`;
-    txt1 += `*◦  المشرف الجديد:* @${m.messageStubParameters[0].split`@`[0]}\n`;
-    txt1 += `*◦  منفذ الترقية:* @${m.sender.split`@`[0]}`;
+    let txt1 = `*Recientemente se ha promovido un miembro a administrador.*\n\n`;
+    txt1 += `*◦  Grupo:* ${groupName}\n`;
+    txt1 += `*◦  Nuevo admin:* @${m.messageStubParameters[0].split`@`[0]}\n`;
+    txt1 += `*◦  Ejecutado por:* @${m.sender.split`@`[0]}`;
     await conn.sendMessage(m.chat, {image: img, caption: txt1, mentions: mentionsString}, {quoted: fkontak2});
   }
 
   if (chat.detect2 && m.messageStubType == 30) {
-    let txt2 = `*💠💠 يبدو انه كان مشرفا متهورا هذه الايام💠💠.*\n\n`;
-    txt2 += `*◦  المجموعة:* ${groupName}\n`;
-    txt2 += `*◦  سحب منه الأشراف:* @${m.messageStubParameters[0].split`@`[0]}\n`;
-    txt2 += `*◦  منفذ السحب:* @${m.sender.split`@`[0]}`;
+    let txt2 = `*Recientemente se ha degradado un administrador a miembro.*\n\n`;
+    txt2 += `*◦  Grupo:* ${groupName}\n`;
+    txt2 += `*◦  Se quitó a:* @${m.messageStubParameters[0].split`@`[0]}\n`;
+    txt2 += `*◦  Ejecutado por:* @${m.sender.split`@`[0]}`;
     await conn.sendMessage(m.chat, {image: img, caption: txt2, mentions: mentionsString}, {quoted: fkontak2});
   }
 
   if (chat.detect2 && m.messageStubType == 27) {
-    let txt3 = `*💠لقد تمت اضافة عضو جديد الى المجموعة!!💠.*\n\n`;
-    txt3 += `*◦  المجموعة:* ${groupName}\n`;
+    let txt3 = `*Recientemente se ha incorporado al grupo un nuevo miembro.*\n\n`;
+    txt3 += `*◦  Grupo:* ${groupName}\n`;
     if (!m.sender.endsWith('@g.us')) {
-      txt3 += `*◦  العضو الجديد:* @${m.messageStubParameters[0].split`@`[0]}\n`;
-      txt3 += `*◦  طريقة الدخول:* @${m.sender.split`@`[0]}`;
+      txt3 += `*◦  Se añadió a:* @${m.messageStubParameters[0].split`@`[0]}\n`;
+      txt3 += `*◦  Ejecutado por:* @${m.sender.split`@`[0]}`;
     } else {
-      txt3 += `*◦ 💠 مرحبا:* @${m.messageStubParameters[0].split`@`[0]}\n`;
+      txt3 += `*◦  Se añadió:* @${m.messageStubParameters[0].split`@`[0]}\n`;
     }
     await conn.sendMessage(m.chat, {image: img, caption: txt3, mentions: mentionsContentM}, {quoted: fkontak2});
   }
 
   if (chat.detect2 && m.messageStubType == 28) {
-    let txt4 = `*💠لقد تم طرد عضو من المجموعة💠.*\n\n`;
-    txt4 += `*◦  المجموعة:* ${groupName}\n`;
+    let txt4 = `*Recientemente se ha eliminado un miembro del grupo.*\n\n`;
+    txt4 += `*◦  Grupo:* ${groupName}\n`;
     if (!m.sender.endsWith('@g.us')) {
-      txt4 += `*◦  العضو:* @${m.messageStubParameters[0].split`@`[0]}\n`;
-      txt4 += `*◦  المنفذ:* @${m.sender.split`@`[0]}`;
+      txt4 += `*◦  Se eliminó a:* @${m.messageStubParameters[0].split`@`[0]}\n`;
+      txt4 += `*◦  Ejecutado por:* @${m.sender.split`@`[0]}`;
     } else {
-      txt4 += `*◦  تمت ازالته:* @${m.messageStubParameters[0].split`@`[0]}\n`;
+      txt4 += `*◦  Se eliminó a:* @${m.messageStubParameters[0].split`@`[0]}\n`;
     }
     await conn.sendMessage(m.chat, {image: {url: pp}, caption: txt4, mentions: mentionsContentM}, {quoted: fkontak2});
   }
@@ -65,13 +65,13 @@ export async function before(m, {conn, participants}) {
     } else {
       ax = 'eliminado';
     }
-    let txt5 = `*لقد 💠 ${ax} عضو مؤخرا💠.*\n\n`;
-    txt5 += `*◦  المجموعة:* ${groupName}\n`;
-    if (ax === 'غادر) {
-      txt5 += `*◦  العضو الذي غادر a:* @${m.messageStubParameters[0].split`@`[0]}\n`;
-      txt5 += `*◦  الذي ازاله/اخرجه:* @${m.sender.split`@`[0]}`;
+    let txt5 = `*Recientemente se ha ${ax} un miembro del grupo.*\n\n`;
+    txt5 += `*◦  Grupo:* ${groupName}\n`;
+    if (ax === 'eliminado') {
+      txt5 += `*◦  Se eliminoó a:* @${m.messageStubParameters[0].split`@`[0]}\n`;
+      txt5 += `*◦  Ejecutado por:* @${m.sender.split`@`[0]}`;
     } else {
-      txt5 += `*◦  هذا صحيح! :* @${m.messageStubParameters[0].split`@`[0]}\n`;
+      txt5 += `*◦  Se salió:* @${m.messageStubParameters[0].split`@`[0]}\n`;
     }
     await conn.sendMessage(m.chat, {image: {url: pp}, caption: txt5, mentions: mentionsContentM}, {quoted: fkontak2});
   }
@@ -83,17 +83,17 @@ export async function before(m, {conn, participants}) {
     } else {
       accion = 'abierto';
     }
-    let txt6 = `*💠لقد تم مؤخرا تعديل اعدادات المجموعة💠.*\n\n`;
-    txt6 += `*◦  المجموعة:* ${groupName}\n`;
-    txt6 += `*◦  التجديد:* ${'```' + accion + '```'}\n`;
-    txt6 += `*◦  المنفذ:* @${m.sender.split`@`[0]}`;
+    let txt6 = `*Recientemente se han modificado los ajustes del grupo.*\n\n`;
+    txt6 += `*◦  Grupo:* ${groupName}\n`;
+    txt6 += `*◦  El grupo se ha:* ${'```' + accion + '```'}\n`;
+    txt6 += `*◦  Ejecutado por:* @${m.sender.split`@`[0]}`;
     await conn.sendMessage(m.chat, {image: {url: pp}, caption: txt6, mentions: mentionsContentM}, {quoted: fkontak2});
   }
 
   if (chat.detect2 && m.messageStubType == 21) {
-    let txt7 = `*💠تم تغيير اسم المجموعة مؤخرًا.💠.*\n\n`;
-    txt7 += `*◦  الاسم الجديد:* ${'```' + groupName + '```'}\n`;
-    txt7 += `*◦  المنفذ:* @${m.sender.split`@`[0]}`;
+    let txt7 = `*Recientemente se ha cambiado el nombre del grupo.*\n\n`;
+    txt7 += `*◦  Nuevo nombre:* ${'```' + groupName + '```'}\n`;
+    txt7 += `*◦  Ejecutado por:* @${m.sender.split`@`[0]}`;
     await conn.sendMessage(m.chat, {image: {url: pp}, caption: txt7, mentions: mentionsContentM}, {quoted: fkontak2});
   }
 } /* Cierre del comando */
