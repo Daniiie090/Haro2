3f6a44f3becc2bc6a574472aa9e79823ae22b5fe
Dnaielle <157284901+Dnaielle@users.noreply.github.com>
2024-02-10 16:44:44 +0300

    Update _antitoxic.js

--- plugins/_antitoxic.js
+++ plugins/_antitoxic.js
@@ -14,12 +14,12 @@ export async function before(m, {isAdmin, isBotAdmin, isOwner}) {
 
   if (isToxic && chat.antiToxic && !isOwner && !isAdmin) {
     user.warn += 1;
-    if (!(user.warn >= 5)) await m.reply('*[❗] ' + `${user.warn == 1 ? `مرحبا صديقي @${m.sender.split`@`[0]}` : `@${m.sender.split`@`[0]}`}, كلمة "${isToxic}" تعتبر سيئة وغير لائقة بك او بي اي شخص لذا لا تقولها مرة اخرى. تحذير: ${user.warn}/5.` + '*', false, {mentions: [m.sender]});
+    if (!(user.warn >= 5)) await m.reply('*[❗] ' + `${user.warn == 1 ? `مرحبا صديقي @${m.sender.split`@`[0]}` : `@${m.sender.split`@`[0]}`}, كلمة "${isToxic}" تعتبر سيئة وغير لائقة بك او بي اي شخص لذا لا تقولها مرة اخرى. تحذير: ${user.warn}/2.` + '*', false, {mentions: [m.sender]});
   }
 
   if (user.warn >= 2) {
     user.warn = 0;
-    await m.reply(`*[❗] مرحبا صديقي @${m.sender.split`@`[0]}, الشتم ممنوع هنا واذا فعلتها مرة اخرى سيتم طردك وشكرا تأدب.*`, false, {mentions: [m.sender]});
+    await m.reply(`*[❗] مرحبا صديقي @${m.sender.split`@`[0]}, الشتم ممنوع هنا  سيتم طردك وشكرا تأدب.*`, false, {mentions: [m.sender]});
     user.banned = true;
     await mconn.conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
     // await this.updateBlockStatus(m.sender, 'block')
