3422a2b36c27140c62c2bcd28629be0e0b741054
Dnaielle <157284901+Dnaielle@users.noreply.github.com>
2024-02-12 18:39:11 +0300

    Update downloader-pinterest.js

--- plugins/downloader-pinterest.js
+++ plugins/downloader-pinterest.js
@@ -1,13 +1,15 @@
-import {pinterest} from '@bochilteam/scraper';
-const handler = async (m, {conn, text, usedPrefix, command}) => {
-  if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] ابحث عن محتوى تريد تحميله صورا من بينترست ${usedPrefix + command} Naksu*`;
-  const json = await pinterest(text);
-  conn.sendFile(m.chat, json.getRandom(), 'error.jpg', `
-*خطأ تحقق غير متوقع*
-${text}
-`.trim(), m);
-};
-handler.help = ['pinterest <keyword>'];
-handler.tags = ['internet'];
-handler.command = /^(بين)$/i;
-export default handler;
+import { pinterest } from '@bochilteam/scraper'
+
+let handler = async(m, { conn, text, usedPrefix, command }) => {
+  if (!text) throw `✳️ ماذا تبحث في بينترست?\n\n📌 Example  : ${usedPrefix + command} Lil Peep`
+  const json = await pinterest(text)
+  conn.sendFile(m.chat, json.getRandom(), 'pinterest.jpg', `
+*▢  Pinterest:*  ${text}
+`.trim(), m)
+}
+handler.help = ['pinterest']
+handler.tags = ['img']
+handler.command = ['بين'] 
+
+export default handler
+
