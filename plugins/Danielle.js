901d38874fca77ceac34ff53202a564a3082fc50
Dnaielle <157284901+Dnaielle@users.noreply.github.com>
2024-02-13 18:36:35 +0300

    Update Danielle.js

--- plugins/Danielle.js
+++ plugins/Danielle.js
@@ -1,11 +1,12 @@
-import { Sticker } from 'wa-sticker-formatter'
+import { Sticker, MessageType } from 'wa-sticker-formatter'
 
 let handler = async (m, { conn }) => {
   
-  let img = 'https://telegra.ph/file/0769a160b0dd4846bfdf7.png ';
-  let packName = 'Naksu/The killer';
-  let authorName = 'Yone/The hunter';
-  let quality = 20;
+  let img = 'https://telegra.ph/file/0769a160b0dd4846bfdf7.png';
+  let packName = 'Naksu'; // يمكنك استبداله بالاسم الذي ترغب في استخدام
+  let authorName = 'Yone/The hunter '; // يمكنك استبداله بالاسم الذي ترغب في استخدامه
+  let quality = 20; // يمكنك تغيير جودة الملصق حسب الحاجة
+  
   let sticker = await createSticker(false, img, packName, authorName, quality);
   
   conn.sendMessage(m.chat, sticker, MessageType.sticker, { quoted: m });
