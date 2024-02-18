02d89a082c9c05ef499e20361541bd0510708dfd
Dnaielle <157284901+Dnaielle@users.noreply.github.com>
2024-02-18 23:51:34 +0300

    Update علم3.js

--- plugins/علم3.js
+++ plugins/علم3.js
@@ -4,23 +4,23 @@ export async function before(m) {
     let id = m.chat
     if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !m.text || !/استخدم.*انسحب/i.test(m.quoted.text) || /.*hhint/i.test(m.text))
         return !0
-    this.tekateki = this.tekateki ? this.tekateki : {}
-    if (!(id in this.tekateki))
+    this.tebakbendera = this.tebakbendera ? this.tebakbendera : {}
+    if (!(id in this.tebakbendera))
         return this.reply(m.chat, '*لقد انتهي هذا السؤال اكتب علم لتظهر أسأله جديده*', m)
-    if (m.quoted.id == this.tekateki[id][0].id) {
+    if (m.quoted.id == this.tebakbendera[id][0].id) {
         let isSurrender = /^(انسحب|surr?ender)$/i.test(m.text)
         if (isSurrender) {
-            clearTimeout(this.tekateki[id][3])
-            delete this.tekateki[id]
+            clearTimeout(this.tebakbendera[id][3])
+            delete this.tebakbendera[id]
             return this.reply(m.chat, '*طلع فاشل و استسلم :( !*', m)
         }
-        let json = JSON.parse(JSON.stringify(this.tekateki[id][1]))
+        let json = JSON.parse(JSON.stringify(this.tebakbendera[id][1]))
         
         if (m.text.toLowerCase() == json.name.toLowerCase().trim()) {
-            global.db.data.users[m.sender].exp += this.tekateki[id][2]
-            this.reply(m.chat, `*❐┃اجـابـة صـحـيـحـة┃✅ ❯*\n\n*❐↞┇الـجـائـزة💰↞${this.tekateki[id][2]} نقطه*`, m)
-            clearTimeout(this.tekateki[id][3])
-            delete this.tekateki[id]
+            global.db.data.users[m.sender].exp += this.tebakbendera[id][2]
+            this.reply(m.chat, `*❐┃اجـابـة صـحـيـحـة┃✅ ❯*\n\n*❐↞┇الـجـائـزة💰↞${this.tebakbendera[id][2]} نقطه*`, m)
+            clearTimeout(this.tebakbendera[id][3])
+            delete this.tebakbendera[id]
         } else if (similarity(m.text.toLowerCase(), json.name.toLowerCase().trim()) >= threshold)
             m.reply(`*لقد كنت علي وشك النجاح*!`)
         else
