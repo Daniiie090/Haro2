768cd5086255fddb81695361ad0e2123853b99d2
Dnaielle <157284901+Dnaielle@users.noreply.github.com>
2024-02-19 23:17:08 +0300

    Update downloader-modapk.js

--- plugins/downloader-modapk.js
+++ plugins/downloader-modapk.js
@@ -1,18 +1,18 @@
 import {search, download} from 'aptoide-scraper';
 const handler = async (m, {conn, usedPrefix: prefix, command, text}) => {
- if (!text) throw `*[❗] Ingrese el nombre de la APK que quiera buscar.*`;
+ if (!text) throw `*[❗] ادخل اسم التطبيق.*`;
   try {    
     const searchA = await search(text);
     const data5 = await download(searchA[0].id);
-    let response = `📲 *Descargador de Aptoide* 📲\n\n📌 *Nombre:* ${data5.name}\n📦 *Package:* ${data5.package}\n🕒 *Última actualización:* ${data5.lastup}\n📥 *Tamaño:* ${data5.size}`
+    let response = `📲 *التنزيل* 📲\n\n📌 *الاسم:* ${data5.name}\n📦 *الملف:* ${data5.package}\n🕒 *تاريخ التحميل:* ${data5.lastup}\n📥 *المساحة:* ${data5.size}`
     await conn.sendMessage(m.chat, {image: {url: data5.icon}, caption: response}, {quoted: m});
  if (data5.size.includes('GB') || data5.size.replace(' MB', '') > 999) {
       return await conn.sendMessage(m.chat, {text: '*[ ⛔ ] El archivo es demasiado pesado por lo que no se enviará.*'}, {quoted: m});
     }
     await conn.sendMessage(m.chat, {document: {url: data5.dllink}, mimetype: 'application/vnd.android.package-archive', fileName: data5.name + '.apk', caption: null}, {quoted: m});
   } catch {
-    throw `*[❗] Error, no se encontrarón resultados para su búsqueda.*`;
+    throw `*[❗] خطأ، لم يتم العثور على نتائج لبحثك.*`;
   }    
 };
-handler.command = /^(apkmod|modapk|dapk2|aptoide|aptoidedl)$/i;
+handler.command = /^(تطبيق|modapk|dapk2|aptoide|aptoidedl)$/i;
 export default handler;
