d9fa0900a47bb71c6d371a838cf828b3790118d2
Dnaielle <157284901+Dnaielle@users.noreply.github.com>
2024-02-19 20:45:32 +0300

    Update menu-caja_fuerte.js

--- plugins/menu-caja_fuerte.js
+++ plugins/menu-caja_fuerte.js
@@ -1,64 +1,105 @@
-const handler = async (m, {conn, usedPrefix}) => {
-  const pp = imagen4;
-  try {
-  } catch (e) {
-  } finally {
-    const name = await conn.getName(m.sender);
-    const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
-    const str = `
-*ミ💖 𝙷𝙾𝙻𝙰 ${taguser} 💖彡*
+import { createHash } from 'crypto'
+import { canLevelUp, xpRange } from '../lib/levelling.js'
+import fetch from 'node-fetch'
+import fs from 'fs'
+const { levelling } = '../lib/levelling.js'
+import moment from 'moment-timezone'
+import { promises } from 'fs'
+import { join } from 'path'
+const time = moment.tz('Egypt').format('HH')
+let wib = moment.tz('Egypt').format('HH:mm:ss')
+//import db from '../lib/database.js'
 
-ㅤㅤ *🗳️<ℂ𝔸𝕁𝔸 𝔽𝕌𝔼ℝ𝕋𝔼/>🔐*
+const dir = ['https://telegra.ph/file/c2e6086f2bf315ec67f05.mp4',];
 
-- 𝙰𝚀𝚄𝙸 𝙿𝚄𝙴𝙳𝙴 𝙶𝚄𝙰𝚁𝙳𝙰𝚁 𝙼𝙴𝙽𝚂𝙰𝙹𝙴𝚂 𝚀𝚄𝙴 𝚀𝚄𝙸𝙴𝚁𝙰𝚂 𝚅𝙴𝚁 𝙼𝙰𝚂 𝚃𝙰𝚁𝙳𝙴
+let handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems}) => {
+    let d = new Date(new Date + 3600000)
+    let locale = 'ar'
+    let week = d.toLocaleDateString(locale, { weekday: 'long' })
+    let date = d.toLocaleDateString(locale, { day: 'numeric', month: 'long', year: 'numeric' })
+    let _uptime = process.uptime() * 1000
+    let uptime = clockString(_uptime)
+let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
+    const videoUrl = pickRandom(dir);
+    const user = global.db.data.users[m.sender];
+  const {money, joincount} = global.db.data.users[m.sender];
+  const {exp, limit, level, role} = 
+    global.db.data.users[m.sender];
+let { min, xp, max } = xpRange(user.level, global.multiplier)
+let username = conn.getName(who)
+let math = max - xp
+let sn = createHash('md5').update(who).digest('hex')
+let totalreg = Object.keys(global.db.data.users).length;
+let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length 
+let more = String.fromCharCode(8206)
+let readMore = more.repeat(900) 
+  const taguser = '@' +  m.sender.split('@s.whatsapp.net')[0];
+    const teks = `${pickRandom([`╭═══〘 ✯✯✯✯✯✯✯✯✯ ═●▬▬▬ஜƸ̵̡⁠Ӝ̵̨̄⁠Ʒஜ▬▬▬●
+    
+━─*˼‏🔃˹ قـسـم التحويلات╿↶*
+━ ── • ⟐ • ── ━
+*✎╎🎴 ⇠〘 .ملصق 〙*    
+*⏎* تحويل الصورة لملصق
+*✎╎🎴 ⇠〘 .مد 〙*    
+*⏎* تحويل الصورة لملصق دائري
+*✎╎©️ ⇠〘.حقوق 〙*    
+*⏎* سرقة ملصق بحقوقك
+*✎╎♻️ ⇠〘 .لصوره 〙*    
+*⏎* تحويل الملصق لصورة
+*✎╎🎞️ ⇠〘 .لفديو 〙*    
+*⏎* تحويل الملصق المتحرك لفيديو
+*✎╎📩 ⇠〘 .تل 〙*    
+*⏎* رفع الصور و الفديوهات الى تليجراف
+*✎╎🐼 ⇠〘 .لكرتون 〙*    
+*⏎* يحول صورك لكرتون مع تحويل الوجه لاستيكر
+*✎╎🏮 ⇠〘 .لانمي 〙*    
+*⏎* تحويل صورك لانمي بالذكاء الاصطناعي
+*✎╎🎙️ ⇠〘 .انطق 〙*    
+*⏎* تحويل النص الى كلام
+*✎╎🎙️ ⇠〘.tm〙*
+*⏎* لتحويل الفيديو الى مقطع صوتي
+━ ── • ⟐ • ── ━
+ ╰⋅ ⋅ ── ⋅ ⋅ ── ✩ ── ⋅ ⋅ ── ⋅ ⋅ ╯
 
-*<𝔸𝔾ℝ𝔼𝔾𝔸ℝ 𝔸 𝕃𝔸 𝕃𝕀𝕊𝕋𝔸/>*
+*~.¸¸ ❝ yone bot❝ ¸¸.~*`])}`.trim();
 
-° ඬ⃟🗳️ _${usedPrefix}agregarmsg *<texto/comando/palabra clave>* (responde a un texto)_
-° ඬ⃟🗳️ _${usedPrefix}agregarvn *<texto/comando/palabra clave>* (responde a una nota de voz)_
-° ඬ⃟🗳️ _${usedPrefix}agregarvideo *<texto/comando/palabra clave>* (responde a un video)_
-° ඬ⃟🗳️ _${usedPrefix}agregaraudio *<texto/comando/palabra clave>* (responde a un audio)_
-° ඬ⃟🗳️ _${usedPrefix}agregarimg *<texto/comando/palabra clave>* (responde a una imagen)_
-° ඬ⃟🗳️ _${usedPrefix}agregarsticker *<texto/comando/palabra clave>* (responde a un sticker)_
-
-*<𝕃𝕀𝕊𝕋𝔸 𝔻𝔼 ℂ𝕆𝕄𝔸ℕ𝔻𝕆𝕊/>*
-
-° ඬ⃟🗳️ _${usedPrefix}listamsg_
-° ඬ⃟🗳️ _${usedPrefix}listavn_
-° ඬ⃟🗳️ _${usedPrefix}listavideo_
-° ඬ⃟🗳️ _${usedPrefix}listaaudio_
-° ඬ⃟🗳️ _${usedPrefix}listaimg_
-° ඬ⃟🗳️ _${usedPrefix}listasticker_
-
-*<𝕍𝔼ℝ 𝕋𝔼𝕏𝕋𝕆𝕊 𝕆 𝔸ℝℂℍ𝕀𝕍𝕆𝕊/>*
-
-° ඬ⃟🗳️ _${usedPrefix}vermsg *<texto/comando/palabra clave>*_
-° ඬ⃟🗳️ _${usedPrefix}vervn *<texto/comando/palabra clave>*_
-° ඬ⃟🗳️ _${usedPrefix}vervideo *<texto/comando/palabra clave>*_
-° ඬ⃟🗳️ _${usedPrefix}veraudio *<texto/comando/palabra clave>*_
-° ඬ⃟🗳️ _${usedPrefix}verimg *<texto/comando/palabra clave>*_
-° ඬ⃟🗳️ _${usedPrefix}versticker *<texto/comando/palabra clave>*_
-
-*<𝔼𝕃𝕀𝕄𝕀ℕ𝔸ℝ/>*
-
-° ඬ⃟🗳️ _${usedPrefix}eliminarmsg *<texto/comando/palabra clave>*_
-° ඬ⃟🗳️ _${usedPrefix}eliminarvn *<texto/comando/palabra clave>*_
-° ඬ⃟🗳️ _${usedPrefix}eliminarvideo *<texto/comando/palabra clave>*_
-° ඬ⃟🗳️ _${usedPrefix}eliminaraudio *<texto/comando/palabra clave>*_
-° ඬ⃟🗳️ _${usedPrefix}eliminarimg *<texto/comando/palabra clave>*_
-° ඬ⃟🗳️ _${usedPrefix}eliminarsticker *<texto/comando/palabra clave>*_`.trim();
-    if (m.isGroup) {
-      conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: m});
-    } else {
-      const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
-      conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: fkontak2});
-    }
-    // conn.sendButton(m.chat, str, wm, pp, [['𝙼𝙴𝙽𝚄 𝙿𝚁𝙸𝙽𝙲𝙸𝙿𝙰𝙻', '/menu']], m, { mentions: [m.sender] })
-  }
+    conn.sendMessage(m.chat, {
+        video: { url: videoUrl },
+        caption: teks,
+        gifPlayback: true,
+        gifAttribution: 0
+    }, { quoted: m });
 };
-handler.help = ['cajafuerte'];
-handler.tags = ['owner'];
-handler.command = /^(cajafuerte)$/i;
-handler.rowner = true;
-handler.fail = null;
+
+handler.command = ['م2'];
+
 export default handler;
+
+function pickRandom(list) {
+    return list[Math.floor(Math.random() * list.length)];
+}
+
+function clockString(ms) {
+    let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+    let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+    let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+    return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
+
+    function ucapan() {
+      const time = moment.tz('Egypt').format('HH')
+      let res = "بداية يوم سعيده ☀️"
+      if (time >= 4) {
+        res = "صباح الخير 🌄"
+      }
+      if (time >= 10) {
+        res = "مساء الخير ☀️"
+      }
+      if (time >= 15) {
+        res = "مساء الخير 🌇"
+      }
+      if (time >= 18) {
+        res = "مساء الخير 🌙"
+      }
+      return res
+    }
