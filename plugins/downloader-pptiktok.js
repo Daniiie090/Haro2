05d7a85182af40ca2656da242330c369d28087bb
Dnaielle <157284901+Dnaielle@users.noreply.github.com>
2024-02-24 17:27:08 +0300

    Update downloader-pptiktok.js

--- plugins/downloader-pptiktok.js
+++ plugins/downloader-pptiktok.js
@@ -3,8 +3,8 @@ const handler = async (m, {conn, text, usedPrefix, command}) => {
   if (text.length < 10) throw `*البلاغ لازم يكون فيه أكثر من ١٠ حروف!*`;
   if (text.length > 1000) throw `*الحد الأقصى للبلاغ ١٠٠٠ حرف!*`;
   const teks = `*❒ بلاغ جديد ❒*\n*≡ صاحب البلاغ :* ${m.sender.split`@`[0]}\n*≡ بلاغه:* *${text}*`;
-  conn.reply('212609395104@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, {contextInfo: {mentionedJid: [m.sender]}});
-  conn.reply('212703388528@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, {contextInfo: {mentionedJid: [m.sender]}});
+  conn.reply('963959649663@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, {contextInfo: {mentionedJid: [m.sender]}});
+  conn.reply('963959649663@s.whatsapp.net', m.quoted ? teks + m.quoted.text : teks, null, {contextInfo: {mentionedJid: [m.sender]}});
   m.reply(`*[ ✔️ ] تم ارسال البلاغ*`);
 };
 handler.help = ['reporte', 'request'].map((v) => v + ' <teks>');
