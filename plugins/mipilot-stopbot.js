e3534925c3d672fa3ce62f5f4f11144bb347af49
Dnaielle <157284901+Dnaielle@users.noreply.github.com>
2024-02-24 16:40:03 +0300

    Update mipilot-stopbot.js

--- plugins/mipilot-stopbot.js
+++ plugins/mipilot-stopbot.js
@@ -1,11 +1,94 @@
-import fs from "fs"
-async function handler(m, {conn, usedPrefix}) {
-   if (conn.user.jid == global.conn.user.jid) return m.reply(`*[❗] No puedes apagar el Bot principal.*`)
-   m.reply(`*[❗] Dejarás de ser un Sub Bot en 5 segundos...*`)
-   conn.fstop = true
-   conn.ws.close()
+import { createHash } from 'crypto'
+import { canLevelUp, xpRange } from '../lib/levelling.js'
+import fetch from 'node-fetch'
+import fs from 'fs'
+const { levelling } = '../lib/levelling.js'
+import moment from 'moment-timezone'
+import { promises } from 'fs'
+import { join } from 'path'
+const time = moment.tz('Egypt').format('HH')
+let wib = moment.tz('Egypt').format('HH:mm:ss')
+//import db from '../lib/database.js'
+
+const dir = ['https://telegra.ph/file/c2e6086f2bf315ec67f05.mp4',];
+
+let handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems}) => {
+    let d = new Date(new Date + 3600000)
+    let locale = 'ar'
+    let week = d.toLocaleDateString(locale, { weekday: 'long' })
+    let date = d.toLocaleDateString(locale, { day: 'numeric', month: 'long', year: 'numeric' })
+    let _uptime = process.uptime() * 1000
+    let uptime = clockString(_uptime)
+let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
+    const videoUrl = pickRandom(dir);
+    const user = global.db.data.users[m.sender];
+  const {money, joincount} = global.db.data.users[m.sender];
+  const {exp, limit, level, role} = 
+    global.db.data.users[m.sender];
+let { min, xp, max } = xpRange(user.level, global.multiplier)
+let username = conn.getName(who)
+let math = max - xp
+let sn = createHash('md5').update(who).digest('hex')
+let totalreg = Object.keys(global.db.data.users).length;
+let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length 
+let more = String.fromCharCode(8206)
+let readMore = more.repeat(900) 
+  const taguser = '@' +  m.sender.split('@s.whatsapp.net')[0];
+    const teks = `${pickRandom([`╭═══〘 ✯✯✯✯✯✯✯✯✯ ═●▬▬▬ஜƸ̵̡⁠Ӝ̵̨̄⁠Ʒஜ▬▬▬●
+    
+━─*˼‏🔃˹ قـسـم الفنانين╿↶*
+━ ── • ⟐ • ── ━
+*✎╎🎴 ⇠〘 .kpop 〙*    
+*⏎* صور عشوائية للفرق الاتية
+Blackpink exo bts
+*✎╎🎴 ⇠〘 .nw 〙*    
+*⏎* صور عشوائية لفرقة newjeans
+*✎╎©️ ⇠〘.بلاك 〙*    
+*⏎* صور عشوائية لفرقة blackpink
+*✎╎♻️ ⇠〘 .ايتزي 〙*    
+*⏎* صور عشوائية لفرقة itzy
+━ ── • ⟐ • ── ━
+ ╰⋅ ⋅ ── ⋅ ⋅ ── ✩ ── ⋅ ⋅ ── ⋅ ⋅ ╯
+
+*~.¸¸ ❝ yone bot❝ ¸¸.~*`])}`.trim();
+
+    conn.sendMessage(m.chat, {
+        video: { url: videoUrl },
+        caption: teks,
+        gifPlayback: true,
+        gifAttribution: 0
+    }, { quoted: m });
+};
+
+handler.command = ['م7'];
+
+export default handler;
+
+function pickRandom(list) {
+    return list[Math.floor(Math.random() * list.length)];
 }
-handler.command = handler.help = ['stop', 'byebot'];
-handler.tags = ['jadibot'];
-handler.owner = true
-export default handler; 
+
+function clockString(ms) {
+    let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+    let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+    let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+    return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
+
+    function ucapan() {
+      const time = moment.tz('Egypt').format('HH')
+      let res = "بداية يوم سعيده ☀️"
+      if (time >= 4) {
+        res = "صباح الخير 🌄"
+      }
+      if (time >= 10) {
+        res = "مساء الخير ☀️"
+      }
+      if (time >= 15) {
+        res = "مساء الخير 🌇"
+      }
+      if (time >= 18) {
+        res = "مساء الخير 🌙"
+      }
+      return res
+           }
